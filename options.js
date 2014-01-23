8542b91cff53ed6a7e23ec57eee18859d0b7c501
Mael <mael.ogier.38@gmail.com>
2014-01-23 17:21:46 +0100

    manifest + options page modification

--- options.js
+++ options.js
@@ -1,42 +1,15 @@
 //Js pour les options de l'extension
 
-function addTag(){
-	var newTag = document.getElementById('newTag').value;
-	var allTags;
-	if(localStorage.getItem('tags'))
-	{
-		allTags=localStorage.getItem('tags')+','+newTag;
-		localStorage.setItem('tags',allTags);
-	}
+function refreshAttributes(){
+	var attributes;
+	if(localStorage.getItem('attributeNamesToGetValues'))
+		attributes = localStorage.getItem('attributeNamesToGetValues');
 	else
-	{
-		localStorage.setItem('tags',newTag);
-	}
-	refreshTags();
-}
+		attributes = 'Aucun';
 
-function clearTags() {
-	localStorage.clear();
-	refreshTags();
-}
-
-function refreshTags(){
-	var tags;
-	if(localStorage.getItem('tags'))
-		tags = localStorage.getItem('tags');
-	else
-		tags = 'Aucun';
-
-	document.getElementById("tags").innerHTML = tags;
+	document.getElementById("attributes").innerHTML = attributes;
 }
 
 window.onload = function(e) {
-
- 	var tagBtn = document.getElementById("tagBtn");
-	tagBtn.onclick = function () { addTag(); };
-
-	var clearBtn = document.getElementById("clearBtn");
-	clearBtn.onclick = function () { clearTags(); };
-
-	refreshTags();
+	refreshAttributes();
 }
