39db1aea3671367e9cf22bb05ac80037dabee21b
Mael <mael.ogier.38@gmail.com>
2014-01-26 23:09:07 +0100

    begin export

--- options.js
+++ options.js
@@ -10,6 +10,16 @@ function refreshAttributes(){
 	document.getElementById("attributes").innerHTML = attributes;
 }
 
+function exportToCsv() {
+	var exportJSON = localStorage.getItem("developer.mozilla.org");
+
+	window.open('data:text/csv;charset=utf-8,' + encodeURI(exportJSON));
+}
+
+        
+
 window.onload = function(e) {
+	var button = document.getElementById("butt");
+    button.onclick = function(){ exportToCsv() };
 	refreshAttributes();
 }
